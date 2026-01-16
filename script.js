676ad2fe375ffd9490b08121b60839486da4acc2
Pyara-Billaa <deepak1pathakdp@gmail.com>
2026-01-16 13:19:16 +0530

    Removed buttons for modes > Add dropdown for modes > Basic UI changes : from side to mi ddle and gap in between

--- script.js
+++ script.js
@@ -1,100 +1,97 @@
 let count = 0;
-let mode = "integers";
-
+let mode;
 
 let containerEl = document.createElement("div");
 containerEl.setAttribute("id", "container-el");
 document.body.appendChild(containerEl);
-document.querySelector("#container-el");
 
-let displayEl = document.createElement("h1");
+let displayEl = document.createElement("input");
 containerEl.appendChild(displayEl);
-displayEl.textContent=0;
 
-let integerBtn = document.createElement("button");
-integerBtn.textContent = "Integers";
-containerEl.appendChild(integerBtn);
+let select = document.createElement("select");
+select.id = "counting";
+containerEl.appendChild(select);
 
-let wholeNumbersBtn = document.createElement("button");
-wholeNumbersBtn.textContent = "Whole Numbers";
-containerEl.appendChild(wholeNumbersBtn);
+let modes = ["integers", "whole", "natural"];
 
-let naturalNumbersBtn = document.createElement("button");
-naturalNumbersBtn.textContent = "Natural Numbers";
-containerEl.appendChild(naturalNumbersBtn);
+modes.forEach((modes) => {
+  const option = document.createElement("option");
+  option.value = modes;
+  option.textContent = modes[0].toUpperCase() + modes.slice(1);
+  select.appendChild(option);
+});
 
+const placeholder = document.createElement("option");
+placeholder.textContent = "Choose mode";
+placeholder.value = "";
+placeholder.disabled = true;
+placeholder.selected = true;
+select.appendChild(placeholder);
+
+let resetContainer = document.createElement("div");
+resetContainer.setAttribute("id", "reset-container");
+document.body.appendChild(resetContainer);
 
 let plusBtn = document.createElement("button");
 plusBtn.setAttribute("id", "plus-btn");
 plusBtn.textContent = "+";
 containerEl.appendChild(plusBtn);
-plusBtn.style.display="none";
+plusBtn.style.display = "none";
 
 let minusBtn = document.createElement("button");
 minusBtn.setAttribute("id", "minus-btn");
 minusBtn.textContent = "-";
 containerEl.appendChild(minusBtn);
-minusBtn.style.display="none";
+minusBtn.style.display = "none";
+
+let resetBtn = document.createElement("button");
+resetBtn.setAttribute("id", "reset-btn");
+resetBtn.textContent = "RESET";
+resetContainer.appendChild(resetBtn);
 
 function enforceRules() {
-    if (mode === "integers") {
-        minusBtn.disabled = false;
-    }
+  if (mode === "integers") {
+    minusBtn.disabled = false;
+  }
 
-    if (mode === "whole") {
-        minusBtn.disabled = (count <= 0);
-    }
+  if (mode === "whole") {
+    minusBtn.disabled = count <= 0;
+  }
 
-    if (mode === "natural") {
-        minusBtn.disabled = (count <= 1);
-    }
+  if (mode === "natural") {
+    minusBtn.disabled = count <= 1;
+  }
 }
 
+select.addEventListener("change", () => {
+  mode = select.value;
+  count = mode === "natural" ? 1 : 0;
+
+  displayEl.value = count;
+  plusBtn.style.display = "inline-block";
+  minusBtn.style.display = "inline-block";
+
+  enforceRules();
+});
 
 plusBtn.addEventListener("click", function () {
-    count += 1;
-    displayEl.textContent = count;
-    enforceRules();
+  count += 1;
+  displayEl.value = count;
+  enforceRules();
 });
 
 minusBtn.addEventListener("click", function () {
-    count -= 1;
-    displayEl.textContent = count;
+  count -= 1;
+  displayEl.value = count;
 
-    enforceRules();
+  enforceRules();
 });
 
-integerBtn.addEventListener("click", function () {
-    mode = "integers";
-    plusBtn.style.display="inline-block";
-    minusBtn.style.display="inline-block";
+resetBtn.addEventListener("click", function () {
+  count = 0;
+  displayEl.value = "";
+  select.value = "";
 
-    wholeNumbersBtn.style.display="none";
-    naturalNumbersBtn.style.display="none";
-})
-
-wholeNumbersBtn.addEventListener("click", function () {
-    mode = "whole";
-    plusBtn.style.display="inline-block";
-    minusBtn.style.display="inline-block";
-
-    if(count<=0){
-        document.querySelector("#minus-btn").disabled=true;
-    }
-
-    integerBtn.style.display="none";
-    naturalNumbersBtn.style.display="none";
-})
-
-naturalNumbersBtn.addEventListener("click", function () {
-    mode = "natural";
-    plusBtn.style.display="inline-block";
-    minusBtn.style.display="inline-block";
-
-    if(count<=0){
-        document.querySelector("#minus-btn").disabled=true;
-    }
-
-    wholeNumbersBtn.style.display="none";
-    integerBtn.style.display="none";
-})
+  plusBtn.style.display = "none";
+  minusBtn.style.display = "none";
+});
